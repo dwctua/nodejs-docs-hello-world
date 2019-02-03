b41c717affb7a19df7b28a7d63b428165c5410e3
dwctua <46290157+dwctua@users.noreply.github.com>
2019-02-03 11:03:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("openshift web hook works!");
+    response.end("openshift web hook retry #1!");
 
 });
 
